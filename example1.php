f4798d6316dbec7d63a8de3d1db0fcc00808ceee
MizbaRoman <92281801+MizbaRoman@users.noreply.github.com>
2022-11-01 13:25:59 +0530

    Javascript testing files

---
+++ example1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    <title>Javascript Example 1</title>
+    </head>
+<body>
+    <h1>First Example In Javascript</h1>
+<script>
+console.log("My name is Mizba");
+
+
+</script>
+</body>
+</html>
